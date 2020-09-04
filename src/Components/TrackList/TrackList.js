fcdb06a38d4f93073d27c6e1663a66fd39d2dbab
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-04 13:20:58 -0500

    mapping to render each track in tracks property

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './TrackList.css';
+import Track from "../Track/Track";
 
 class TrackList extends Component {
     render() {
         return (
             <div className="TrackList">
-                {/*You will add a map method that renders a set of Track components*/}
+                {this.props.tracks.map(track => <Track track={track} key={track.id}/>)}
             </div>
         );
     }
