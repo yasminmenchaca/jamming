0d11e97449b82cd5a5b135535a71f9a2e5b833e8
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-04 13:18:52 -0500

    added filler search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,15 @@ import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}]
+        }
+    }
+
     render() {
         return (
             <div>
@@ -13,7 +22,7 @@ class App extends Component {
                 <div className="App">
                     <SearchBar/>
                     <div className="App-playlist">
-                        <SearchResults/>
+                        <SearchResults searchResults={this.state.searchResults}/>
                         <Playlist/>
                     </div>
                 </div>
