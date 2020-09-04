f33eca1e68d1298a933f86407b116c1c55ad026b
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-04 14:54:55 -0500

    added onAdd props

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,7 @@ class TrackList extends Component {
     render() {
         return (
             <div className="TrackList">
-                {this.props.tracks.map(track => <Track track={track} key={track.id}/>)}
+                {this.props.tracks.map(track => <Track track={track} key={track.id} onAdd={this.props.onAdd}/>)}
             </div>
         );
     }
