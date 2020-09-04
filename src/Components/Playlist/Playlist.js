bd10435f02e6c3b4f273327179bb1827b094c418
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-04 14:54:04 -0500

    added playlistTracks props

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -8,7 +8,7 @@ class Playlist extends Component {
             <div>
                 <div className="Playlist">
                     <input value="New Playlist"/>
-                    {/*<TrackList/>*/}
+                    <TrackList tracks={this.props.playlistTracks}/>
                     <button className="Playlist-save">SAVE TO SPOTIFY</button>
                 </div>
             </div>
