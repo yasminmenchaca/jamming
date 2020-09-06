04fe35ef27979aeb961b2bc397e130de2157151e
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-06 08:18:56 -0500

    in progress

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -27,6 +27,10 @@ class App extends Component {
         }
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
+        this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlaylist = this.savePlaylist.bind(this);
+        this.search = this.search.bind(this);
+
     }
 
     addTrack(track) {
@@ -44,15 +48,29 @@ class App extends Component {
         this.setState({playlistTracks: tracks})
     }
 
+    updatePlaylistName(name) {
+        this.setState({playlistName: name});
+    }
+
+    savePlaylist() {
+        const trackURI = this.state.playlistTracks.map(track => trackURI);
+    }
+
+    search(term) {
+        console.log(term);
+    }
+
     render() {
         return (
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    <SearchBar/>
+                    <SearchBar onSearch={this.search}/>
                     <div className="App-playlist">
                         <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-                        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+                        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
+                                  onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}
+                                  onSave={this.savePlaylist}/>
                     </div>
                 </div>
             </div>
